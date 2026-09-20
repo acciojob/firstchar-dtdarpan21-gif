75cf85a7e856a8828ab59815a445110fc3af89c3
dtdarpan21-gif <dtdarpan21@gmail.com>
2026-09-20 18:04:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 function firstChar(text) {
 	let trimmedText = text.trim();
+	   if (trimmedText.length === 0) {
+        return "";
+    }
+
 	return trimmedText.charAt(0);
   // your code here
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-const text = prompt("Enter text:");
+//const text = prompt("Enter text:");
 alert(firstChar(text));
